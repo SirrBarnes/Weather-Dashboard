c85cf3f847696bfa21cb517cdb35643c55a5cce4
SirrBarnes <torressergio1357@gmail.com>
2024-08-12 18:11:18 -0400

    changes to routes and service files

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,72 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/:city', (req, res) => {
   // TODO: GET weather data from city name
+  router.get('/api/weather/', async (_req, res) => {
+    try {
+      const cityName = req.params.city;
+      
+      (cityName);
+      const cities = await WeatherService.getWeatherForCity(cityName);
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  });
   // TODO: save city to search history
+
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (req, res) => {
+  try {
+    const historyResults = req.params;
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('history/:id', async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({msg: 'Search History City ID required'});
+    }
+    await HistoryService.removeCity(req.params.id);
+    res.json({success: 'City successfully removed from search history'});
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
 
 export default router;
+
+
+// import { Router } from 'express';
+// const router = Router();
+
+// // import HistoryService from '../../service/historyService.js';
+// // import WeatherService from '../../service/weatherService.js';
+
+// // TODO: POST Request with city name to retrieve weather data
+// router.post('/', (req, res) => {
+//   // TODO: GET weather data from city name
+//   // TODO: save city to search history
+// });
+
+// // TODO: GET search history
+// router.get('/history', async (req, res) => {});
+
+// // * BONUS TODO: DELETE city from search history
+// router.delete('/history/:id', async (req, res) => {});
+
+// export default router;
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,30 +2,87 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  lat: string;
+  lon: string;
+};
 
 // TODO: Define a class for the Weather object
+class Weather {
+  temp: string;
+  wind: string;
+  humidity: string;
+
+  constructor(temp: string, wind: string, humidity: string) {
+    this.temp = temp;
+    this.wind = wind;
+    this.humidity = humidity;
+  }
+};
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+  private apiKey?: string;
+  private cityName?: string;
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    
+    this.apiKey = process.env.API_KEY || '';
+
+    this.cityName = '';
+  }
+
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+
+  }
+
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const currentCoordinates = locationData;
+
+    return currentCoordinates;
+  }
+
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  // private buildGeocodeQuery(): string {
+
+  // }
   // TODO: Create buildWeatherQuery method
   // private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
   // private async fetchAndDestructureLocationData() {}
+
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {
+    try {
+      const coordURL = await fetch(`${process.env.API_BASE_URL}/data/2.5/weather?lat=${coordinates.lat}}&lon=${coordinates.lon}&appid=${process.env.API_KEY}`);
+
+      
+
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {
+
+  }
+
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    // const weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=$${lon}&appid=${process.env.API_KEY}`;
+  }
+
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {
+    const cityUrl = `${process.env.API_BASE_URL}/data/2.5/weather?q=${city}&appid=${process.env.API_Key}`
+
+    return cityUrl;
+  }
 }
 
 export default new WeatherService();
